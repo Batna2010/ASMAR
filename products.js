35802a16026bd8e683e32c738412bc2203c739ea
Batna2010 <116046263+Batna2010@users.noreply.github.com>
2022-10-20 16:21:40 +0200

    Add files via upload

---
+++ products.js
@@ -0,0 +1,16 @@
+const products = [
+	{
+		id: 2,
+		logo:'img/asmar.jpg',
+		image: 'img/products/coach.jpg',
+		image2: 'img/products/coachr.png',
+		
+		title: ' دورة فن الإلقاء والخطابة بدون أوراق ⏩ ',
+		price1: 5500,
+		price: 3000,
+		description: '  محاور الدورة '   ,
+		phone: 0549217739,
+	},
+
+	
+];
